41d8b2b717c2714d1d29227169375d5bc07858de
arke-off <edificios.seguridad@outlook.com>
2024-04-19 03:25:20 -0300

    agregando codigo

---
+++ python_lenguaje/1-sintaxisBasica.py
@@ -0,0 +1,33 @@
+#imprimiendo texto
+texto = "Hola mundo"
+print (texto)
+
+#declaracion de variables
+a=2
+b=5
+print (a+b)
+
+valorDecimal = 10.3234
+
+print (valorDecimal)
+
+#podemos realizar multiples asignaciones e imprimirlas
+i, j, k = 4,3,1
+print(i, j, k)
+
+## Definimos una cariable booleana True con mayuscula
+imprimir = True
+
+
+##ejercicio de impresión de variables con diferente formato (imprimir=verdadero)
+x = ("El valor de y + 2 es igual a :")
+y = 20 + 2 
+if imprimir:
+    print(x, y)
+
+'''
+comentarios
+aqui puede ir todo el texto 
+a modo de comentario
+
+'''
---
+++ python_lenguaje/2-estructuraIF.py
@@ -0,0 +1,8 @@
+##conjunto de reglas que definen como se tiene que escribir el código
+##python es sensible a las indentaciones
+
+a=10
+if a == 2:
+    print ("Es 10")
+else :
+    print ("No es 10")
